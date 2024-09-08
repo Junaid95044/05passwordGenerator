bf8d159b7a2db97d3e07a1ddc196351c649db132
Junaid Kambho <m.junaidarshad95@gmail.com>
2024-09-08 23:26:41 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : "/05passwordGenerator/",
+  base : "/05passwordGenerator",
   plugins: [react()],
 })
